3e19d48544e7c9009c8a565c59086478ce51952b
robrobrob <robbiegabbie@gmail.com>
2017-02-14 14:23:20 -0500

    second work

--- src/Feb82017WedSandL.java
+++ src/Feb82017WedSandL.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -9,6 +11,12 @@ import java.util.Scanner;
 */
 public class Feb82017WedSandL {
 
+    public static void main(String[] args) {
+        problem1();
+        problem2();
+        problem3();
+        problem4();
+    }
     public static void problem1()
     {
         String firstName;
@@ -28,7 +36,7 @@ public class Feb82017WedSandL {
         lastName = keyScan.nextLine();
 
         String middleI = middleInitial.toString();
-        System.out.println("Your full name is:" + firstName + middleI + lastName);
+        System.out.println("Your full name is: " + firstName +" "+ middleI +" "+ lastName);
         // you get to finish the rest of this method.
 
 
@@ -42,7 +50,21 @@ public class Feb82017WedSandL {
         System.out.print("Enter a sentence or phrase with a ! somewhere inside it: ");
         response = keyScan.nextLine();
 
-        response.
+        if (response.contains("!")){
+            int i;
+            int totalContent = response.length();
+            for(i = 0; i<=totalContent;i++){
+                char found = response.charAt(i);
+                char exPoint = '!';
+                if(found == exPoint){
+                    int index = i;
+                    int indexPlus = i+1;
+                    System.out.println(response.substring(0,i));
+                    System.out.println(response.substring(indexPlus,response.length()));
+                    i = totalContent+1;
+                }
+            }
+        }
         // you get to finish the rest of this method.
 
     }
@@ -50,6 +72,21 @@ public class Feb82017WedSandL {
     public static void problem3()
     {
         // you get to create the contents of this method
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter anything!:");
+        String output = input.nextLine();
+        int lengthOfOutput = output.length();
+        char firstLetter = output.charAt(output.toCharArray());
+        String first = String.valueOf(firstLetter);
+        char lastLetter = output.charAt(output.length());
+        String last = String.valueOf(lastLetter);
+        System.out.println(lengthOfOutput);
+        System.out.println(first);
+        System.out.println(last);
+
+    }
+
+    public static void problem4(){
 
     }
 
